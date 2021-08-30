9c582f4423d6ef97b299e36323aadcf359e26e34
kyeimuda <kyeibernard01@gmail.com>
2021-08-30 12:10:46 -0700

    postive or negativ

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,10 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+*main - Entry point
+*Return:Always 0 (Success)
+*/ 
 int main(void) /**
 *main - Entry point
 */
