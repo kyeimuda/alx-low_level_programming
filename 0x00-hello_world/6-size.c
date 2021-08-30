82ce37a0fcf1465b846988ddcfddbb9e1c3d4aba
kyeimuda <kyeibernard01@gmail.com>
2021-08-30 08:27:26 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,5 @@ printf("Size of a long int: %li byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
 printf("Size of a float: %li byte(s)\n", sizeof(float));
 
-  
 return (0);
 }
