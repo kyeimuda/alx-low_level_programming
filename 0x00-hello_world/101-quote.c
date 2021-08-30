24bd42494945b71bb9b48836e80c3d4098ba1fdf
kyeimuda <kyeibernard01@gmail.com>
2021-08-30 14:13:31 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0(succes)
+*/
+
+int main(void) /**
+*main - Enrty point
+*/
+
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
+
