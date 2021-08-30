60d5b40637706dd58a21a16e9e45e2caca02ca6c
kyeimuda <kyeibernard01@gmail.com>
2021-08-30 07:11:22 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-n *main - Entry point
+ *main - Entry point
  *Return: Always 0(succes)
 */
 
@@ -10,7 +10,7 @@ int main(void) /**
 */
 
 {
-printf("with proper grammar, but the outcome is piece of art\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
