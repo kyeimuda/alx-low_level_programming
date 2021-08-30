8b6bacc1f751edaf84e0eb88733e0695a9fc4fdc
kyeimuda <kyeibernard01@gmail.com>
2021-08-30 07:05:29 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+n *main - Entry point
+ *Return: Always 0(succes)
+*/
+
+int main(void) /**
+*main - Enrty point
+*/
+
+{
+printf("with proper grammar, but the outcome is piece of art\n");
+
+return (0);
+}
+
