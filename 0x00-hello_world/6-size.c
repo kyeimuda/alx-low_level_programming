45c9cfb5880d1e2170888a83d0005d0c880491d9
kyeimuda <kyeibernard01@gmail.com>
2021-08-30 08:13:41 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+/**
+ *main - Entry point
+ *Return: Always 0(succes)
+*/
+
+int main(void) /**
+*main - Enrty point
+*/
+
+{
+printf ("Size of a char: %li byte(s)\n", sizeof(char));
+printf ("Size of a int: %li byte(s)\n", sizeof(int));
+printf ("Size of a long int: %li byte(s)\n", sizeof(long int));
+printf ("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+printf ("Size of a float: %li byte(s)\n", sizeof(float));
+
+  
+return (0);
+}
