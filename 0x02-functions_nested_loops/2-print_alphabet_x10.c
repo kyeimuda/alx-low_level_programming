285ba5aa66b5bc0599d17c7c565bca143159c942
kyeimuda <kyeibernard01@gmail.com>
2021-08-31 13:28:19 -0700

    print x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void);
+int main(void)
+
+{
+int ch = 'a' , j;
+{
+return (1);
+}
+for( j = '0' ; j <= 10 ; j++)
+{
+for( ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar(j);
+}
+return (0);
+}
