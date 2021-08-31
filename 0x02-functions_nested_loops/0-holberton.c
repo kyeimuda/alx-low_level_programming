7640e6880ced3865593ab727a694e93c3a34173c
kyeimuda <kyeibernard01@gmail.com>
2021-08-31 09:51:35 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,15 +7,14 @@
 
 int main(void)
 {
-putchar('H');
-putchar('o');
-putchar('l');
-putchar('b');
-putchar('e');
-putchar('r');
+putchar('_');
+putchar('p');
+putchar('u');
 putchar('t');
-putchar('o');
-putchar('n');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
 putchar('\n');
 
 return (0);
