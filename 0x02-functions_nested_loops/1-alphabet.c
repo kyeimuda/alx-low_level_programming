c003a28a09c9fca0642c10b9e2cb1a3fb2e7e725
kyeimuda <kyeibernard01@gmail.com>
2021-08-31 11:24:05 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int ch = 'a';
+for  ( ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+
+return (0);
+}
