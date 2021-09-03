84cbf6de7d41af11a0a0cc9270bd4f2d22d230f5
kyeimuda <kyeibernard01@gmail.com>
2021-09-02 19:19:30 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ if (a > b || b > c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b > a || a > c)
 {
-largest = b;
+largest = a;
 }
 else
 {
-largest = c;
+largest = a;
 }
 
 return (largest);
