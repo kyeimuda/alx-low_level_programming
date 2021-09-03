1cb97de4ae84253a5a85b117d91e0c9ae0e9d7cc
kyeimuda <kyeibernard01@gmail.com>
2021-09-03 12:17:46 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main"
+#include <ctype.h>
+
+/**
+*isupper - checks a character whether the passed character is uppercase letter
+*@c: The character to print
+*Return : non zero value if true
+*/
+int ispper(int c)
+{
+return;
+}
