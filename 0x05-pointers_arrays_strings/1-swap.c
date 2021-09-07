ff753d0ebd1e6260b20fd199847da1491970517e
kyeimuda <kyeibernard01@gmail.com>
2021-09-07 10:59:32 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - to swap the values of two integers
- * @a - integer to swap
- * @b - integers to swap
+ * @a: integer to swap
+ * @b: integers to swap
  * Return: Always 0
  */
 
