942c6688f85a0fc53231f2624a529d28cd040584
kyeimuda <kyeibernard01@gmail.com>
2021-09-09 10:53:55 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp : compares two strings
+ * @s1: a string
+ * @s2: a string
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+result = strcmp(s1, s2);
+return (result);
+}
