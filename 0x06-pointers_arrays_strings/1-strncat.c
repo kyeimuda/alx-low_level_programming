20ab3515cfa5aff27094a289f1d9c28925337587
kyeimuda <kyeibernard01@gmail.com>
2021-09-09 05:40:15 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two string using at most n bytes
+ * @dest: destination
+ * @src: source
+ * @n: maximum number of characters to be appended
+ */
+
+char *_strncat(char *dest, char* src, int n)
+{
+strncat(dest, src, n);
+return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@
  */
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *scr, int n);
 
 #endif
