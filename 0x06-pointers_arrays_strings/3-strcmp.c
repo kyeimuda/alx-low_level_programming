ffa3001a7b0d6645b4d57229ef793e541e5c25f2
kyeimuda <kyeibernard01@gmail.com>
2021-09-09 11:39:56 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,6 @@
 int _strcmp(char *s1, char *s2)
 {
 int result;
-if (result = strcmp(s1, s2);)
-{
+ result = strcmp(s1, s2);
+ result = strcmp(s2, s1);
 return (result);
-}
-if (result = strcmp(s2, s1));
-{
-return (result);
-}
-return (result);
-}
