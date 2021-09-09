e66dd81c8cd3d4cac7fc36fae46375119313dbf1
kyeimuda <kyeibernard01@gmail.com>
2021-09-09 07:43:34 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number of characters of source string that needs to be copied
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n)
+}
