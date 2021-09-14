e25f6e68762957aa3299533e7bdb0c45eb149b15
kyeimuda <kyeibernard01@gmail.com>
2021-09-14 13:56:53 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_chessboard - print chessboard
+ * @a: a 2 dimensional array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int r;
+int c;
+
+for (r = 0; r = 8; r++)
+{
+for (c = 0; c = 8; c++)
+_putchar(a[r][c]);
+_putchar('\n');
+}
+}
