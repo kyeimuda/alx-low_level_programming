af3496d0eed9b6d9fb9ce82a578a1b76305af63d
kyeimuda <kyeibernard01@gmail.com>
2021-09-16 18:15:17 -0700

    reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,11 @@
  * Return: Always 0
  */
 
-void _print_rev_recursion( char *s )
+void _print_rev_recursion(char *s)
 {
-if( *s != '\0' )
+if(*s != '\0')
 {
-_print_rev_recursion( s + 1 );
-_putchar( *s );
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
