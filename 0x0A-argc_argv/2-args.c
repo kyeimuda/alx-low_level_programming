eb56a3360e06615bb83678eb368862fe25c928d1
kyeimuda <kyeibernard01@gmail.com>
2021-09-17 19:22:13 -0700

    2nd args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,18 +2,17 @@
 #include "main.h"
 
 /**
- * main - will print its name followed by a new\
- line
+ * main - will print its name followed by a new line
  * @argc: counts number of arguments typed
  * @argv: array that points to typed arguments
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int i;
-
-for (i = 0; 1 < argc; i++)
+while (argc--)
+{
 printf("%s\n", *argv++);
+}
 return (0);
 }
