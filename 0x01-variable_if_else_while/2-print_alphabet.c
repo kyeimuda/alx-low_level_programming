665e71b02820f9d93d5749a21cc9c07c4aa842c8
kyeimuda <kyeibernard01@gmail.com>
2021-11-10 17:03:54 -0800

    printing alphabets in lower case

---
+++ 0x01-variable_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+char low_alpha;
+
+for (low_alpha = 'a'; low_alpha <= 'z'; low_alpha++)
+putchar(low_alpha);
+putchar('\n');
+return (0);
+}
