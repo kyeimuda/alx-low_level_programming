c141e2284c1c83d3c5ca772cf6d182cfd94138e4
kyeimuda <kyeibernard01@gmail.com>
2021-11-23 18:45:38 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,14 @@ for (n = 48 ; n <= 57 ; n++)
 {
 for (m = 48 ; m <= 57 ; m++)
 {
-putchar(n);
-putchar(m);
-if (n == m)
+if  (n == m)
 {
 continue;
+}
+else
+{
+putchar(n);
+putchar(m);
 if ((n != 57) || (m != 57))
 {
 putchar(',');
