d19d580cccfd8b4659a6f0f4f5fa49302bed54e8
kyeimuda <kyeibernard01@gmail.com>
2021-11-28 18:47:35 -0800

    betty code style errorr fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,15 @@
 #include "main.h"
 
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 void print_alphabet(void)
 {
 int ch = 'a';
-for( ch = 'a'; ch <= 'z'; ch++)
+for(ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
 return;
