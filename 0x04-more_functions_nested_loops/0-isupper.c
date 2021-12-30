e9833fbd078e37ced1899239fc203c436831de1d
kyeimuda <kyeibernard01@gmail.com>
2021-12-29 18:52:15 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,20 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
-*isupper - checks a character whether the passed character is uppercase letter
-*@c: The character to print
-*Return : non zero value if true
-*/
-int isupper(int c);
+ *isupper - checks a character whether the passed character is uppercase letter
+ *@c: The character to print
+ *Return : non zero value if true
+ */
+int isupper(int c)
+{
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
